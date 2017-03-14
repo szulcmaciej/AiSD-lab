8db6aa8d80f662bebb830cacfdbfd5a8aaa3c58d
szulcmaciej <szulcmaciej1@gmail.com>
2017-03-14 17:19:44 +0100

    CustomArrayList prawie gotowe
    nie testowane CustomArrayList, iterator do poprawy

--- airlineroute/foundation/src/main/java/com/asid/foundation/datastructure/list/CustomArrayList.java
+++ airlineroute/foundation/src/main/java/com/asid/foundation/datastructure/list/CustomArrayList.java
@@ -8,32 +8,37 @@ import java.util.Iterator;
  * @param <T>
  */
 public class CustomArrayList<T> extends AbstractCustomListAdapter<T> {
-    /* (TODO Lab No. 1) Please introduce a sensible implementation for storage*/
+    static final int DEFAULT_INITIAL_CAPACITY = 20;
+    static final double RESIZING_FACTOR = 1.3;
+
+    private int initial_capacity;
+    private Object[] array;
+    private int size;
+
 
     public CustomArrayList() {
+        this(DEFAULT_INITIAL_CAPACITY);
     }
 
     public CustomArrayList(int initialCapacity) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation for storage*/
+        initial_capacity = initialCapacity;
+        clear();
     }
 
     @Override
     public int size() {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return 0;
+        return size;
     }
 
 
     @Override
     public boolean isEmpty() {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return false;
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return false;
+        return indexOf(o) != -1;
     }
 
     @Override
@@ -44,70 +49,127 @@ public class CustomArrayList<T> extends AbstractCustomListAdapter<T> {
 
     @Override
     public boolean add(T t) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return false;
+        add(size, t);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return false;
+        int index = indexOf(o);
+        if(index != -1){
+            remove(index);
+        }
+        return index != -1;
     }
 
     @Override
     public void clear() {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
+        array = new Object[initial_capacity];
+        size = 0;
     }
 
     @Override
-    public T get(int index) {
+    public T get(int index) throws IndexOutOfBoundsException{
         /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return null;
+        checkOutOfBounds(index);
+        return (T) array[index];
     }
 
     @Override
-    public T set(int index, T element) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return null;
+    public T set(int index, T element) throws IndexOutOfBoundsException{
+        checkOutOfBounds(index);
+        T old_value = (T) array[index];
+        array[index] = element;
+        return old_value;
     }
 
     @Override
-    public void add(int index, T element) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
+    public void add(int index, T element) throws IndexOutOfBoundsException {   //insert
+        if(index < 0 || index > size) throw new IndexOutOfBoundsException();
+        ensureCapacity(size + 1);
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = element;
+        size++;
     }
 
     @Override
-    public T remove(int index) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return null;
+    public T remove(int index) throws IndexOutOfBoundsException {
+        checkOutOfBounds(index);
+        T value = (T) array[index];
+        int copy_from = index + 1;
+        if(copy_from < size){
+            System.arraycopy(array, copy_from, array, index, size - copy_from);
+        }
+        size--;
+        return value;
     }
 
     @Override
     public int indexOf(Object o) {
-        /* (TODO Lab No. 1) Please introduce a sensible implementation */
-        return 0;
+        int i = 0;
+        while(i < size && !o.equals(array[i])){
+            i++;
+        }
+        return i < size ? i : -1;
+    }
+
+    private void ensureCapacity(int capacity){
+        if(capacity > 0.9 * array.length){
+            int new_length = (int) (capacity * RESIZING_FACTOR);
+            adjustLength(new_length);
+        }
+        else if(capacity < 0.6 * array.length){
+            int new_length = (int) (capacity * RESIZING_FACTOR);
+            adjustLength(new_length);
+        }
+    }
+
+    private void adjustLength(int newLength){
+        Object[] new_array = new Object[newLength];
+        System.arraycopy(array, 0, new_array, 0, size);
+        array = new_array;
+    }
+
+    private void checkOutOfBounds(int index) throws IndexOutOfBoundsException{
+        if(index < 0 || index >= size) throw new IndexOutOfBoundsException();
     }
 
     /**
      * Iterator for CustomArrayList
      */
     private class CustomArrayListIterator<E> implements Iterator<E> {
+        int current_index;
+
+        public CustomArrayListIterator() {
+            current_index = -1;
+        }
 
         @Override
         public boolean hasNext() {
              /* (TODO Lab No. 1) Please introduce a sensible implementation */
-            return false;
+            if(current_index < size - 1)
+                return  true;
+            else
+                return false;
         }
 
         @Override
         public E next() {
              /* (TODO Lab No. 1) Please introduce a sensible implementation */
-            return null;
+            E return_value = null;
+            if(hasNext())
+            {
+                current_index++;
+                return_value = (E) array[current_index];
+            }
+            return return_value;
         }
 
         @Override
         public void remove() {
              /* (TODO Lab No. 1) Please introduce a sensible implementation */
+            CustomArrayList.this.remove(current_index);
+            current_index--;
         }
     }
 }
