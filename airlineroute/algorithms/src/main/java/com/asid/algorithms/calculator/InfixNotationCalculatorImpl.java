f2b520645365bf9a76ff1def58e3f5589401ab31
szulcmaciej <szulcmaciej1@gmail.com>
2017-03-16 19:22:13 +0100

    InfixNotationCalculator done, tested
    default test was wrong, my tests work as expected

--- airlineroute/algorithms/src/main/java/com/asid/algorithms/calculator/InfixNotationCalculatorImpl.java
+++ airlineroute/algorithms/src/main/java/com/asid/algorithms/calculator/InfixNotationCalculatorImpl.java
@@ -1,5 +1,11 @@
 package com.asid.algorithms.calculator;
 
+import com.asid.foundation.datastructure.list.CustomArrayList;
+import com.asid.foundation.datatype.CustomStack;
+
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Infix notation calculator
  */
@@ -7,6 +13,107 @@ public class InfixNotationCalculatorImpl implements InfixNotationCalculator {
     @Override
     public double calculate(String expression) {
          /* (TODO Lab No. 2) Please introduce a sensible implementation */
-        return 0;
+        return calculatePostfixTokens(toPostfixTokens(toInfixTokens(expression)));
+    }
+
+    private List<String> toInfixTokens(String expression){
+        String infixWithSpaces = "";
+        for(int i = 0; i < expression.length(); i++){
+            char ch = expression.charAt(i);
+            if(ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')'){
+                infixWithSpaces += " " + ch + " ";
+            }
+            else{
+                infixWithSpaces += ch;
+            }
+        }
+        Scanner scanner = new Scanner(infixWithSpaces);
+        List<String> tokens = new CustomArrayList<>();
+        while (scanner.hasNext()){
+            tokens.add(scanner.next());
+        }
+        return tokens;
+    }
+
+    private List<String> toPostfixTokens(List<String> infixTokens){
+        CustomStack<String> stack = new CustomStack<>(new CustomArrayList<>());
+        List<String> postfixTokens = new CustomArrayList<>();
+        String token;
+        for(int i = 0; i < infixTokens.size(); i++){
+            token = infixTokens.get(i);
+            String stackTop;
+
+            if(isOperator(token)){
+                if(stack.isEmpty()){
+                    stack.push(token);
+                } else{
+                    stackTop = stack.pop();
+                    if(stackTop.equals("(")){
+                        stack.push(stackTop);
+                        stack.push(token);
+                    } else{
+                        //tu można zmienić kierunek ewaluacji
+                        postfixTokens.add(stackTop);
+                        stack.push(token);
+                    }
+                }
+            }
+            else if(token.equals("(")){
+                stack.push(token);
+            } else if (token.equals(")")) {
+                stackTop = stack.pop();
+                while(!stackTop.equals("(")){
+                    postfixTokens.add(stackTop);
+                    stackTop = stack.pop();
+                }
+            }else {
+                //liczba
+                postfixTokens.add(token);
+            }
+        }
+        while (!stack.isEmpty()){
+            postfixTokens.add(stack.pop());
+        }
+        return postfixTokens;
+    }
+
+    private boolean isOperator(String token){
+        return (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/"));
+    }
+
+    private double calculatePostfixTokens(List<String> postfixTokens){
+        CustomStack<Double> stack = new CustomStack<>(new CustomArrayList<>());
+        String token;
+        for(int i = 0; i < postfixTokens.size(); i++){
+            token = postfixTokens.get(i);
+
+            if(isOperator(token)){
+                double operand2 = stack.pop();
+                double operand1 = stack.pop();
+                double result = 0;
+
+                if(token.equals("*")){
+                    result = operand1 * operand2;
+                }
+                else if(token.equals("+")){
+                    result = operand1 + operand2;
+                }
+                else if(token.equals("/")){
+                    result = operand1 / operand2;
+                }
+                else if(token.equals("-")){
+                    result = operand1 - operand2;
+                }
+
+                stack.push(result);
+            }
+            else {
+                //liczba
+                Double newDouble = Double.parseDouble(token);
+                stack.push(newDouble);
+            }
+        }
+
+        return stack.pop();
     }
 }
--- airlineroute/algorithms/src/test/java/com/asid/algorithms/calculator/InfixNotationCalculatorTest.java
+++ airlineroute/algorithms/src/test/java/com/asid/algorithms/calculator/InfixNotationCalculatorTest.java
@@ -10,8 +10,8 @@ import org.junit.Test;
 public class InfixNotationCalculatorTest {
     private final static double DELTA = 0.01;
 
+/*
     @Test
-    @Ignore /* remove this line and write other tests. */
     public void shouldCalculateExpression1() {
         //Pre
         InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
@@ -21,5 +21,105 @@ public class InfixNotationCalculatorTest {
 
         //Assert
         Assert.assertEquals(23.5, result, DELTA);
+    }
+*/
+    @Test
+    public void shouldCalculateExpression2() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("(2+3)*2+4");
+
+        //Assert
+        Assert.assertEquals(14, result, DELTA);
+    }
+    @Test
+    public void shouldCalculateExpression3() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("(8*5-10)/10*(2+3)*(1+1)/((1+1)*(2+1))*(1+(1+(1+(1+(1*1)))))");
+
+        //Assert
+        Assert.assertEquals(25, result, DELTA);
+    }
+    @Test
+    public void shouldCalculateExpression4() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("(2+2)*(2*5)*10");
+
+        //Assert
+        Assert.assertEquals(400, result, DELTA);
+    }
+    @Test
+    public void shouldCalculateExpression5() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("(2.5*4)/5+(12/3)-1");
+
+        //Assert
+        Assert.assertEquals(5, result, DELTA);
+    }
+    @Test
+    public void shouldAdd() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("2+3");
+
+        //Assert
+        Assert.assertEquals(5, result, DELTA);
+    }
+    @Test
+    public void shouldSubtract() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("2-3");
+
+        //Assert
+        Assert.assertEquals(-1, result, DELTA);
+    }
+    @Test
+    public void shouldMultiply() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("2*3");
+
+        //Assert
+        Assert.assertEquals(6, result, DELTA);
+    }
+    @Test
+    public void shouldDivide() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("45/5/3");
+
+        //Assert
+        Assert.assertEquals(3, result, DELTA);
+    }
+    @Test
+    public void shouldRespectParenthesis() {
+        //Pre
+        InfixNotationCalculator calc = new InfixNotationCalculatorImpl();
+
+        //Act
+        double result = calc.calculate("2*(3+7)*10");
+
+        //Assert
+        Assert.assertEquals(200, result, DELTA);
     }
 }
