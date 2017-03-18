56f39c288f61d987bf9e864f1463f2492e5b69d9
szulcmaciej <szulcmaciej1@gmail.com>
2017-03-18 22:48:57 +0100

    QuickSort WIP
    not working so far :)

--- airlineroute/algorithms/src/main/java/com/asid/algorithms/sorting/QuickSortService.java
+++ airlineroute/algorithms/src/main/java/com/asid/algorithms/sorting/QuickSortService.java
@@ -11,6 +11,43 @@ import java.util.List;
 public class QuickSortService extends AbstractSortService {
     public static <T> SortResultDs<T> sort(List<T> list, Comparator<? super T> comparator) {
           /* (TODO Lab No. 4) Please introduce a sensible implementation */
-        throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
+        //throw new UnsupportedOperationException(StandardMessages.METHOD_NOT_IMPLEMENTED);
+
+        long startTime = System.currentTimeMillis();
+
+        if(!list.isEmpty()){
+            quickSort(list, comparator, 0, list.size() - 1);
+        }
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+
+        SortResultDs<T> sortResult = new SortResultDs<>();
+        sortResult.setResult(list);
+        sortResult.setEstimatedTime(elapsedTime);
+
+        return sortResult;
+    }
+
+    private static <T> void quickSort(List<T> list, Comparator<? super T> comparator, int begIndex, int endIndex){
+        if(begIndex < endIndex){
+            int pivot = begIndex;
+            int wall = pivot;
+            int i = begIndex + 1;
+            while (i <= endIndex){ //czy na pewno?
+                if(comparator.compare(list.get(i), list.get(pivot)) < 0){
+                    wall++;
+                    exch(list, i, wall);
+                }
+            }
+            exch(list, pivot, wall);
+
+            quickSort(list, comparator, begIndex, wall);
+            if(wall < endIndex){
+                quickSort(list, comparator, wall + 1, endIndex);
+            }
+        }
+
+
     }
 }
