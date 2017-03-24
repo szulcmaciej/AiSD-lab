73ddc775dff54a8623f4ef4dd97395fee9260c63
szulcmaciej <szulcmaciej1@gmail.com>
2017-03-24 19:53:03 +0100

    MergeSortTest improved
    Added some additional tests.

--- airlineroute/algorithms/src/test/java/com/asid/algorithms/sorting/MergeSortServiceTest.java
+++ airlineroute/algorithms/src/test/java/com/asid/algorithms/sorting/MergeSortServiceTest.java
@@ -3,6 +3,7 @@ package com.asid.algorithms.sorting;
 import com.asid.foundation.datastructure.list.CustomArrayList;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -44,5 +45,69 @@ public class MergeSortServiceTest {
         assertEquals(7.7, sortedList.get(6), DELTA);
         assertEquals(7.7, sortedList.get(7), DELTA);
     }
+
+
+    @Test
+    public void shouldReturnSameListOnSortedList(){
+        //Pre
+        List<Double> list = new ArrayList<>();
+        Comparator<Double> naturalComparator = Comparator.naturalOrder();
+        list.add(6.5);
+        list.add(4.0);
+        list.add(3.8);
+        list.add(7.7);
+        list.add(1.1);
+        list.add(7.7);
+        list.add(2.2);
+        list.add(3.8);
+
+
+        java.util.Collections.sort(list, naturalComparator);
+
+        List<Double> listCopy = new ArrayList<>();
+        for(Double d : list){
+            listCopy.add(d);
+        }
+
+        //Act
+        MergeSortService.sort(list, naturalComparator);
+
+        //Assert
+        assertEquals(8, list.size());
+        for (int i = 0; i < listCopy.size(); i++) {
+            assertEquals(list.get(i), listCopy.get(i), DELTA);
+        }
+    }
+
+    @Test
+    public void shouldSortListOfSimilarElements(){
+        //Pre
+        List<Double> list = new CustomArrayList<>();
+        Comparator<Double> naturalComparator = Comparator.naturalOrder();
+        list.add(6.5);
+        list.add(6.5);
+        list.add(6.5);
+        list.add(6.5);
+        list.add(6.5);
+        list.add(6.5);
+        list.add(6.5);
+        list.add(6.5);
+
+
+        //Act
+        List<Double> sortedList = MergeSortService.sort(list, naturalComparator).getResult();
+
+        //Assert
+        assertEquals(8, sortedList.size());
+        assertEquals(6.5, sortedList.get(0), DELTA);
+        assertEquals(6.5, sortedList.get(1), DELTA);
+        assertEquals(6.5, sortedList.get(2), DELTA);
+        assertEquals(6.5, sortedList.get(3), DELTA);
+        assertEquals(6.5, sortedList.get(4), DELTA);
+        assertEquals(6.5, sortedList.get(5), DELTA);
+        assertEquals(6.5, sortedList.get(6), DELTA);
+        assertEquals(6.5, sortedList.get(7), DELTA);
+    }
+
 
 }
